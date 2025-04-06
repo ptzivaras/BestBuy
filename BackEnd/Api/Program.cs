256b616e96b36514584d1f227b34e5dacba55f5d
ptzivaras <82588420+ptzivaras@users.noreply.github.com>
2025-04-06 14:05:06 +0300

    Initial commit

---
+++ BackEnd/Api/Program.cs
@@ -0,0 +1,19 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+//app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
